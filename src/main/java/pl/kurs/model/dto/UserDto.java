859367ee4d6cf4ea69a12daa95bf9084aa92618d
bartekk189 <bartekk189@outlook.com>
2023-06-26 19:11:04 +0200

    ad

--- src/main/java/pl/kurs/model/dto/UserDto.java
+++ src/main/java/pl/kurs/model/dto/UserDto.java
@@ -2,12 +2,14 @@ package pl.kurs.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.kurs.model.entity.User;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDto {
     private long id;
     private String userName;
