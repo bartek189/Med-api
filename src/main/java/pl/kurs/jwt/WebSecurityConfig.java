830d0907e393e73efa42dc374c417d5cd2094d12
bartekk189 <bartekk189@outlook.com>
2023-06-26 19:23:51 +0200

    wwwe

--- src/main/java/pl/kurs/jwt/WebSecurityConfig.java
+++ src/main/java/pl/kurs/jwt/WebSecurityConfig.java
@@ -44,9 +44,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+
+
         http
                 .csrf()
                 .disable()
+                .headers().frameOptions()
+                .disable()
+                .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/authenticate")
                 .permitAll()
